a43ebbff4f205c1aa776a753844e43979aef7ea8
iosyyy <626797813@qq.com>
2021-06-28 09:02:25 +0800

    init

---
+++ src/main/java/com/management/Application.java
@@ -0,0 +1,15 @@
+package com.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 靖鸿宣
+ * @since 2021/6/28
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class,args);
+  }
+}
